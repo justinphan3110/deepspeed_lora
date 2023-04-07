245de22f22a8ce6defcfd7531b092737a120a3ac
Long Phan <justinphan3110@gmail.com>
2023-04-07 00:22:58 -0700

    init scripts to train multi nodes

--- scripts/alpaca_30B.sh
+++ scripts/alpaca_30B.sh
@@ -3,13 +3,13 @@
 source /opt/rh/devtoolset-10/enable
 export TRANSFORMERS_CACHE='hf_cache'
 
-srun --nodes=1 --gpus-per-node=8 deepspeed ../src/train_alpaca_lora.py \
+srun --nodes=2 --gpus-per-node=8 deepspeed ../src/train_alpaca_lora.py \
     --model_name_or_path /data/private_models/cais_models/llama/llama_hf_weights/llama-30b \
     --train_file ../data/alpaca_data_cleaned.json \
     --output_dir out/new_alpaca_30b \
     --num_train_epochs 10 \
-    --per_device_train_batch_size 8 \
-    --per_device_eval_batch_size 4 \
+    --auto_find_batch_size \
+    --per_device_eval_batch_size 1 \
     --evaluation_strategy "no" \
     --save_strategy "epoch" \
     --fp16 \
@@ -18,6 +18,5 @@ srun --nodes=1 --gpus-per-node=8 deepspeed ../src/train_alpaca_lora.py \
     --logging_steps 10 \
     --deepspeed ../configs/ds_zero3.json \
     --lr_scheduler_type "cosine" \
-    --gradient_checkpointing \
     --tf32 True \
-    --report_to none
+    --report_to tensorboard
