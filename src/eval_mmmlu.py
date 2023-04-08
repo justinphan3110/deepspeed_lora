ec085f294153e53d8430f69fa67b33380fbbf7d1
Long Phan <justinphan3110@gmail.com>
2023-04-08 16:10:42 -0700

    init mmmlu eval

--- src/eval_mmmlu.py
+++ src/eval_mmmlu.py
@@ -16,9 +16,8 @@ from typing import Dict
 
 from peft import PeftModel
 import transformers
-from ..src.alpaca_lora.prompter import Prompter
 
-# from ..src.alpaca_lora.prompter import P
+from alpaca_lora.prompter import Prompter
 from tqdm import tqdm
 from datasets import Dataset
 
